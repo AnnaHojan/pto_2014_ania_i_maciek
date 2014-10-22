0eded7e2563a41b7428b74d1773779cd510dc84e
MsSkatefish <anna.hojan@gmail.com>
2014-10-22 20:03:47 +0200

    Poprawka zadania z 2 ćwiczeń: korekcja- dodanie obsługi obrazów w skali odcieni szarości.

--- src/core/transformations/correction.cpp
+++ src/core/transformations/correction.cpp
@@ -12,6 +12,7 @@ Correction::Correction(PNM* img, ImageViewer* iv) :
 
 PNM* Correction::transform()
 {
+
     float shift  = getParameter("shift").toFloat();
     float factor = getParameter("factor").toFloat();
     float gamma  = getParameter("gamma").toFloat();
@@ -21,7 +22,6 @@ PNM* Correction::transform()
 
     PNM* newImage = new PNM(width, height, image->format());
 
-	//qDebug() << Q_FUNC_INFO << "Not implemented yet!";
 	for (int i = 0; i <= 255; i++)
 	{
 		LUT[i] = i;
@@ -41,20 +41,32 @@ PNM* Correction::transform()
 		}
 	}
 	// Iterate over image space
-	for (int x = 0; x<width; x++)
-		for (int y = 0; y<height; y++)
-		{
-		QRgb pixel = image->pixel(x, y); // Getting the pixel(x,y) value
-
-		int r = qRed(pixel);    // Get the 0-255 value of the R channel
-		int g = qGreen(pixel);  // Get the 0-255 value of the G channel
-		int b = qBlue(pixel);   // Get the 0-255 value of the B channel
-		r = LUT[r];
-		g = LUT[g];
-		b = LUT[b];
-		QColor newPixel = QColor(r, g, b);
-		newImage->setPixel(x, y, newPixel.rgb());
-		}
-
+	if (image->format() == QImage::Format_RGB32)
+	{
+		for (int x = 0; x < width; x++)
+			for (int y = 0; y < height; y++)
+			{
+				QRgb pixel = image->pixel(x, y); // Getting the pixel(x,y) value
+				int r = qRed(pixel);    // Get the 0-255 value of the R channel
+				int g = qGreen(pixel);  // Get the 0-255 value of the G channel
+				int b = qBlue(pixel);   // Get the 0-255 value of the B channel
+				r = LUT[r];
+				g = LUT[g];
+				b = LUT[b];
+				QColor newPixel = QColor(r, g, b);
+				newImage->setPixel(x, y, newPixel.rgb());
+			}
+	}
+	else if (image->format() == QImage::Format_Indexed8)
+	{
+		for (int x = 0; x < width; x++)
+			for (int y = 0; y < height; y++)
+			{
+				QRgb pixel = image->pixel(x, y); // Getting the pixel(x,y) value
+				int v = qGray(pixel);    
+				v = LUT[v];
+				newImage->setPixel(x, y, v);
+			}
+	}
     return newImage;
 }

aa6e8b228b8fba5bcb44e0d9a0e3dc3a4daabac7
Maciej Brusiło <drzejzi@gmail.com>
2014-10-29 23:27:16 +0100

    4.4 - Rozmywanie liniowe

--- src/core/transformations/blur_linear.cpp
+++ src/core/transformations/blur_linear.cpp
@@ -18,7 +18,47 @@ PNM* BlurLinear::transform()
 
     math::matrix<float> mask(maskSize, maskSize);
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+
+	/*
+		dzieje siê dziwna rzecz:
+		przyk³ad zamieszczony na eduwiki rozmywa siê nie w t¹ stronê co powinien
+		kiedy jednak maska zostaje przystosowana do poprawnego przekszta³cenia przyk³adu, pozosta³e przekszta³cenia
+		wykonuj¹ siê w obrócone o 90 stopni
+		mo¿e wiêc przyk³ad jest niepoprawny?
+		z tego powodu na wszelki wypadek dok³adamy poni¿szy parametr
+	*/
+	bool wersja_przeksztalcajaca_prawidlowo_przyklad_z_eduwiki = false;
+
+
+
+
+	int licznik = 0;
+	for (int i = 0; i < maskSize; i++)
+	{
+		for (int j = 0; j < maskSize; j++)
+		{
+			float x = tmpMask.at(licznik).toDouble();
+			licznik++;
+			if (!wersja_przeksztalcajaca_prawidlowo_przyklad_z_eduwiki)
+				mask(i, j) = x;
+			else
+				mask(j, i) = x;
+		}
+	}
+	if (normalize)
+	{
+		float suma = sum(mask);
+		for (int i = 0; i < maskSize; i++)
+		{
+			for (int j = 0; j < maskSize; j++)
+			{
+				mask(i, j) = mask(i, j) / suma;
+			}
+		}
+	}
+	
+	if (!wersja_przeksztalcajaca_prawidlowo_przyklad_z_eduwiki)
+		mask = reflection(mask);
 
     return convolute(mask, RepeatEdge);
 }

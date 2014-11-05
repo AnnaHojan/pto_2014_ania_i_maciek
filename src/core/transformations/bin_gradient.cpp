5b30beceba1306cb669acc4d1cf8048ba18384ca
MsSkatefish <anna.hojan@gmail.com>
2014-11-05 20:10:59 +0100

    5.1, 5.2 - Binaryzacja

--- src/core/transformations/bin_gradient.cpp
+++ src/core/transformations/bin_gradient.cpp
@@ -17,7 +17,42 @@ PNM* BinarizationGradient::transform()
 
     PNM* newImage = new PNM(width, height, QImage::Format_Mono);
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	double licznik=0;
+	double mianownik = 0;
+	QRgb pixel, pixel1, pixel2, pixel3, pixel4;
+	int g1, g2;
+	for (int x = 0; x<width; x++)
+		for (int y = 0; y<height; y++)
+		{
+			pixel = getPixel(x, y, RepeatEdge);
+			pixel1 = getPixel(x+1, y, RepeatEdge);
+			pixel2 = getPixel(x-1, y, RepeatEdge);
+			pixel3 = getPixel(x, y+1, RepeatEdge);
+			pixel4 = getPixel(x, y-1, RepeatEdge);
+
+			g1 = qGray(pixel1) - qGray(pixel2);
+			g2 = qGray(pixel3) - qGray(pixel4);
+			if (g1 > g2)
+			{
+				licznik = licznik + (g1*qGray(pixel));
+				mianownik += g1;
+			}
+			else
+			{
+				licznik = licznik + (g2*qGray(pixel));
+				mianownik += g2;
+			}
+		}
+	
+	double threshold = licznik / mianownik;
+	for (int x = 0; x<width; x++)
+		for (int y = 0; y<height; y++)
+		{
+		QRgb pixel = image->pixel(x, y); 
+
+		int v = qGray(pixel);    
+		newImage->setPixel(x, y, v < threshold ? Qt::color0 : Qt::color1);
+		}
 
     return newImage;
 }

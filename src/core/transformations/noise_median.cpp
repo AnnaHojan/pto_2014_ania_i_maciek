5ac85f72e2afc64a2f26c46f2175ed45015980cf
Maciej Brusiło <drzejzi@gmail.com>
2014-11-12 21:49:25 +0100

    6.1. Odszumianie - Filtr medianowy

--- src/core/transformations/noise_median.cpp
+++ src/core/transformations/noise_median.cpp
@@ -17,16 +17,55 @@ PNM* NoiseMedian::transform()
 
     PNM* newImage = new PNM(width, height, image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	if (image->format() == QImage::Format_Indexed8) {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int value = getMedian(i, j, LChannel);
+				newImage->setPixel(i, j, value);
+			}
+		}
+	}
+	else if (image->format() == QImage::Format_RGB32) {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int valueR = getMedian(i, j, RChannel);
+				int valueG = getMedian(i, j, GChannel);
+				int valueB = getMedian(i, j, BChannel);
+				QColor newPixel = QColor(valueR, valueG, valueB);
+				newImage->setPixel(i, j, newPixel.rgb());
+			}
+		}
+	}
 
     return newImage;
+}
+
+// pomocnicza funkcja wykorzystywana w quicksorcie
+int compare(const void *a, const void *b)
+{
+	int value = *(int*)a - *(int*)b;
+	return value;
 }
 
 int NoiseMedian::getMedian(int x, int y, Channel channel)
 {
     int radius = getParameter("radius").toInt();
+	int rozmiar = (radius * 2) + 1;
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	math::matrix<float> window(rozmiar, rozmiar);
+	window = getWindow(x, y, rozmiar, channel, RepeatEdge);
 
-    return 0;
+	int *tablica = new int[rozmiar * rozmiar];
+
+	for (int i = 0; i < rozmiar; i++) {
+		for (int j = 0; j < rozmiar; j++) {
+			tablica[(i*rozmiar) + j] = window(i, j);
+		}
+	}
+
+	qsort(tablica, rozmiar * rozmiar, sizeof(int), compare);
+
+	int mediana = tablica[(rozmiar * rozmiar) / 2];
+	delete[] tablica;
+	return mediana;
 }

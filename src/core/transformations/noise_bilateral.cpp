17d09266c4bdaef50713f2fae0f7cc5b0793850d
Maciej Brusiło <drzejzi@gmail.com>
2014-11-13 01:42:13 +0100

    6.2. Odszumianie - filtr bilateralny

--- src/core/transformations/noise_bilateral.cpp
+++ src/core/transformations/noise_bilateral.cpp
@@ -21,28 +21,71 @@ PNM* NoiseBilateral::transform()
     sigma_r = getParameter("sigma_r").toInt();
     radius = sigma_d;
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	if (image->format() == QImage::Format_Indexed8) {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int value = calcVal(i, j, LChannel);
+				newImage->setPixel(i, j, value);
+			}
+		}
+	}
+	else if (image->format() == QImage::Format_RGB32) {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int valueR = calcVal(i, j, RChannel);
+				int valueG = calcVal(i, j, GChannel);
+				int valueB = calcVal(i, j, BChannel);
+				QColor newPixel = QColor(valueR, valueG, valueB);
+				newImage->setPixel(i, j, newPixel.rgb());
+			}
+		}
+	}
 
     return newImage;
 }
 
 int NoiseBilateral::calcVal(int x, int y, Channel channel)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	float licznik = 0;
+	float mianownik = 0;
 
-    return 0;
+	int rozmiar = (radius * 2) + 1;
+
+	math::matrix<float> window(rozmiar, rozmiar);
+	window = getWindow(x, y, rozmiar, channel, RepeatEdge);
+
+	for (int i = 0; i < rozmiar; i++) {
+		for (int j = 0; j < rozmiar; j++) {
+			QPoint pij(x + i - radius, y + j - radius);
+			QPoint pxy(x, y);
+			licznik += (window(i, j) * colorCloseness(window(i, j), window(radius, radius)) * spatialCloseness(pij, pxy)); // I(x, y) -> window(radius, radius), czyli środek okna
+			mianownik += (colorCloseness(window(i, j), window(radius, radius)) * spatialCloseness(pij, pxy));
+		}
+	}
+
+	float calcVal = licznik / mianownik;
+
+	return calcVal;
 }
 
 float NoiseBilateral::colorCloseness(int val1, int val2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	float value = exp(
+		-(
+		((val1 - val2)*(val1 - val2)) / (2 * (sigma_r * sigma_r))
+		)
+		);
 
-    return 0;
+	return value;
 }
 
 float NoiseBilateral::spatialCloseness(QPoint point1, QPoint point2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	float value = exp(-(
 
-    return 0;
+		(((point1.x() - point2.x())*(point1.x() - point2.x())) + ((point1.y() - point2.y())*(point1.y() - point2.y()))) / (2 * (sigma_d*sigma_d))
+
+		));
+
+	return value;
 }

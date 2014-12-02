b17c0fd50e098f2ff9874fb67374d765c784f271
Maciej Brusiło <drzejzi@gmail.com>
2014-12-02 23:03:11 +0100

    9.1. Mapa wysokości

--- src/core/transformations/map_height.cpp
+++ src/core/transformations/map_height.cpp
@@ -17,7 +17,33 @@ PNM* MapHeight::transform()
 
     PNM* newImage = new PNM(width, height, QImage::Format_Indexed8);
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	// copypasta z conversion_grayscale::transform
+	if (image->format() == QImage::Format_Mono)
+	{
+		// Iterate over image space
+		for (int x = 0; x<width; x++)
+			for (int y = 0; y<height; y++)
+			{
+			QColor color = QColor::fromRgb(image->pixel(x, y)); // Getting the pixel(x,y) value
+
+			newImage->setPixel(x, y, color == Qt::white ? PIXEL_VAL_MAX : PIXEL_VAL_MIN);
+			}
+	}
+	else // if (image->format() == QImage::Format_RGB32)
+	{
+		// Iterate over image space
+		for (int x = 0; x<width; x++)
+			for (int y = 0; y<height; y++)
+			{
+			QRgb pixel = image->pixel(x, y); // Getting the pixel(x,y) value
+
+			int r = qRed(pixel);    // Get the 0-255 value of the R channel
+			int g = qGreen(pixel);  // Get the 0-255 value of the G channel
+			int b = qBlue(pixel);   // Get the 0-255 value of the B channel
+			int new_color = (int)((0.6*r) + (0.3*g) + (0.1*g));
+			newImage->setPixel(x, y, new_color);
+			}
+	}
 
     return newImage;
 }
